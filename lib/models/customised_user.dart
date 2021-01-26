41a4849cc375470713a93dcb1cb81c84e630befa
Paulina Tao <paulina@example.com>
2021-01-26 03:17:46 +0000

    basic db setup

--- lib/models/customised_user.dart
+++ lib/models/customised_user.dart
@@ -1,6 +1,28 @@
 class CustomisedUser {
   final String uid;
+  bool state = false;
 
   CustomisedUser(this.uid);
 
+  String get userId {
+    return uid;
+  }
+
+  bool get infectionState {
+    return state;
+  }
+
+  set infectionState(bool currentState) {
+    state = currentState;
+  }
+
+  // factory CustomisedUser.fromJson(Map<String, dynamic> json) => CustomisedUser(
+  //   uid: json["id"],
+  //   state: json["infection_state"],
+  // );
+
+  Map<String, dynamic> toMap() => {
+    "id": uid,
+    "state": state,
+  };
 }
--- lib/screens/user/user.dart
+++ lib/screens/user/user.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:hexcolor/hexcolor.dart';
+import 'package:provider/provider.dart';
+import 'package:wifi_scanning_flutter/models/customised_user.dart';
 import 'package:wifi_scanning_flutter/screens/user/setting.dart';
+
+import '../../user_data.dart';
 
 class User extends StatefulWidget {
   @override
@@ -12,9 +16,30 @@ class User extends StatefulWidget {
 class UserHomePage extends State<User> {
   final Color kingsBlue = HexColor('#0a2d50');
   final Color kingsPearlGrey = HexColor("cdd7dc");
+  //get a database object
+  UserData userData = UserData();
+
+  @override
+  void initState(){
+   super.initState();
+   //initialise the user database
+   //there will only be 1 database for the app
+   userData.initialiseDatabase().then((value) {
+     print("User database is initialised.");
+   });
+  }
 
   @override
   Widget build(BuildContext context){
+    final user = Provider.of<CustomisedUser>(context);
+    void toggleInfectionState(){
+      setState(() {
+        user.infectionState = !user.infectionState;
+        userData.insertUserInfo(user);
+        print(userData);
+      });
+    }
+
     return Scaffold(
         appBar: AppBar(
           title: Text("Personal Page"),
@@ -27,7 +52,16 @@ class UserHomePage extends State<User> {
                 })
           ]
         ),
+      body: Center(
+        child: Text('The current user infection state is: ${user.infectionState}'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.priority_high),
+        onPressed: () {
+          toggleInfectionState();
+        },
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
-
 }
