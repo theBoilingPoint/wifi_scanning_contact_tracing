479d7892de7d5994a1e5ab3a276952b58b9175ed
theBoilingPoint <774677341@qq.com>
2021-03-31 02:04:25 +0100

    Unused sign in functions removed.

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -36,32 +36,6 @@ class AuthService {
     }
   }
 
-  //sign in with email & password
-  Future signInWithEmailAndPassword(String email, String password) async {
-    try{
-      UserCredential result = await _auth.signInWithEmailAndPassword(email: email, password: password);
-      User user = result.user;
-      return _customisedUserFromUser(user);
-    }
-    catch(e){
-      print(e.toString());
-      return null;
-    }
-  }
-
-  //register with email & password
-  Future registerWithEmailAndPassword(String email, String password) async {
-    try{
-      UserCredential result = await _auth.createUserWithEmailAndPassword(email: email, password: password);
-      User user = result.user;
-      return _customisedUserFromUser(user);
-    }
-    catch(e){
-      print(e.toString());
-      return null;
-    }
-  }
-
   //sign out
   Future signOut() async {
     try {
